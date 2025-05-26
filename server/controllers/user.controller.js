06a17f488df315d828f6e6cb5ccb403270a170ef
Sufyan Shaikh <sufyans1353@gmail.com>
2025-05-26 11:30:07 +0530

    some changes in server

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,63 +1,66 @@
-import {  Webhook } from "svix"
+import { Webhook } from "svix"
 import userModel from "../models/user.model.js";
 // API controller function to manage clerk user with db
 // http://localhost:4000/api/user/webhooks
 
-const clerkWebhooks = async(req,res) =>{
+const clerkWebhooks = async (req, res) => {
     try {
+        console.log("🛎️ Clerk webhook triggered")
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"]
+        const evt = await whook.verify(req.body.toString("utf8"), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
         })
 
-        const {data,type} = req.body;
+        const { data, type } = evt;
 
         switch (type) {
-            case "user.created":{
+            case "user.created": {
                 const userData = {
-                    clerkId :data.id,
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.first_name,
-                    lastName : data.last_name,
-                    photo : data.image_url,
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url,
                 }
                 await userModel.create(userData)
-                res.json({})
+                res.json({ message: "Successfully created..." })
                 break;
             }
-                
-            case "user.updated":{
-                 const userData = {
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.first_name,
-                    lastName : data.last_name,
-                    photo : data.image_url,
+
+            case "user.updated": {
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url,
                 }
                 await userModel.findOneAndUpdate(
-                    {clerkId:data.id},
+                    { clerkId: data.id },
                     userData
                 )
-                res.json({})
+                res.json({ message: "Successfully updated..." })
+
                 break;
             }
-                
-            case "user.deleted":{
-                await userModel.findOneAndDelete({clerkId:data.id})
-                res.json({})
+
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id })
+                res.json({ message: "Successfully deleted..." })
+
                 break;
             }
-                
-        
+
+
             default:
                 break;
         }
     } catch (error) {
         console.log(error.message);
-        res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
-export {clerkWebhooks}
+export { clerkWebhooks }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 import userRouter from './routes/user.route.js'
+import bodyParser from "body-parser"
 
 //App Config
 const PORT = process.env.PORT || 4000
@@ -11,9 +12,11 @@ const app = express()
 await connectDB()
 
 //Initialize Middlewares
+app.use('/api/user/webhooks', bodyParser.raw({ type: 'application/json' }))
 app.use(express.json())
 app.use(cors())
 
+// ✅ Handle Clerk webhook raw body separately
 
 //API routes
 app.get('/',(req,res)=>{
